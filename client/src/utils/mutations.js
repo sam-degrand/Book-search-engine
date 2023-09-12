e7453073c2f736f89fc1f80ff9cdce1075d86c6b
Sam DeGrandchamp <degrandchamps@gmail.com>
2023-09-12 19:01:04 -0400

    save and remove book mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,39 @@
+import { gql } from '@apollo/client';
+
+export const SAVE_BOOK = gql`
+  mutation SaveBook($input: BookInput!) {
+    saveBook(input: $input) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation RemoveBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
